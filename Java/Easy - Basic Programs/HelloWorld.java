2fcbc97aae383c12c8fd9fcb937b36c907ae936c
ShawnArunjith <shawnarun09@gmail.com>
2024-02-12 23:23:10 +0530

    FEAT : Printing HelloWorld Developed

---
+++ Java/Easy - Basic Programs/HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * Author : ShawnArunjith
+ * Email : .............
+ * Date : ...................
+ * 
+ * Description : Printing Hello World
+ * 
+ * 
+ */
+
+
+class HelloWorld
+{
+    public static void main(String []args)
+    {
+        System.out.println("Hello World");
+    }
+};
