708880abf205ea4168cd8ad60fb57aaa2b79b061
Keerthika04 <jkeerthi144@gmail.com>
2024-02-20 15:29:43 +0530

    Feat: Ternary Operators and normal If-else

---
+++ JavaScript/Easy - Basic Programs/Ternary_Operators.js
@@ -0,0 +1,18 @@
+/*
+ * Author : Keerthika04
+ * Email : jkeerthi144@gmail.com
+ * Date : 20th of Feb 2024
+ * Description : Ternary Operators and normal If-else
+ */
+
+let num = 6;
+
+//If-else
+if (num > 5) {
+  console.log(num + " is more than 5.");
+} else {
+  console.log(num + " is less than 5.");
+}
+
+//Ternary Operator
+num > 5 ? console.log(num + " is more than 5.") : console.log(num + " is less than 5.");
