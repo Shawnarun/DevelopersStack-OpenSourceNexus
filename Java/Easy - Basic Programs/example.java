48343923ee20aaf1d1d84a65e7783420d362e954
thushaonline <rajthusha99@gmail.com>
2024-02-21 18:49:46 +0530

    FEAT :Int Variable Developed

---
+++ Java/Easy - Basic Programs/example.java
@@ -0,0 +1,15 @@
+/*
+* Author : thushaonline
+* Gmail : rajthusha99@gmail.com
+* Date : 20.02.2024
+* Description :  Int Variable
+*/
+public class example{
+    public static void main (String [] args){
+        int[] numbers = {1,2,3,4,5};
+        for (int i = 0; i<numbers.length; int++ ){
+            numbers [i]+=3;}
+
+            System.out.println(numbers[2]);
+        }
+}
