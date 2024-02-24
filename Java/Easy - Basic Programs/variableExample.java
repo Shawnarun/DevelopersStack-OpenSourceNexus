4c8820d200452a000a3b454dd72c728c5b81973d
thushaonline <rajthusha99@gmail.com>
2024-02-24 20:46:41 +0530

    FEAT : Variables Example Developed

---
+++ Java/Easy - Basic Programs/variableExample.java
@@ -0,0 +1,20 @@
+/*
+* Author : thushaonline
+* Gmail : rajthusha99@gmail.com
+* Date : 24.02.2024
+* Description :  Variables Example
+*/
+
+public class VariablesExample {
+    public static void main(String[] args) {
+        int number = 10;
+        double pi = 3.14;
+        boolean isJavaFun = true;
+        String greeting = "Hello";
+
+        System.out.println("Number: " + number);
+        System.out.println("PI: " + pi);
+        System.out.println("Is Java fun? " + isJavaFun);
+        System.out.println("Greeting: " + greeting);
+    }
+}
