97c95da68491c62400684d13dfbb65aaaa5a80c5
thushaonline <rajthusha99@gmail.com>
2024-03-06 22:26:01 +0530

    FEAT : SumToNumbers Developed

--- Java/Medium - Competitive Programming/sumCalculator.java
+++ Java/Medium - Competitive Programming/sumCalculator.java
@@ -2,22 +2,24 @@
 * Author : thushaonline
 * Gmail : rajthusha99@gmail.com
 * Date : 06.03.2024
-* Description :  Abstract Class
+* Description :  SumToNumbers
 */
 
-package MyData;
-class MyName{
-String name;
-MyName(){
-name=null;
-}
-void show(){
-System.out.println(name);
-}
-}
-public class DP{
-public static void main(String args[]){
-MyName ob=new MyName();
-ob.show();
-}
+import java.util.Scanner;
+
+public class SumCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter the first number:");
+        int num1 = scanner.nextInt();
+        
+        System.out.println("Enter the second number:");
+        int num2 = scanner.nextInt();
+        
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        
+        scanner.close();
+    }
 }
