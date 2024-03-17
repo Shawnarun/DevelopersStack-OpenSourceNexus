31ba1993aaaf50f2c6d2116e4daa48fabdc51d8e
E024664 <E024664@esoft.academy>
2024-03-17 16:01:19 +0530

    FEAT : For-loop

---
+++ JavaScript/Easy - Basic Programs/For-Loop.js
@@ -0,0 +1,31 @@
+/*
+ * Author : AkmalAzeem
+ * Email : akmalazeem2003@gmail.com
+ * Date : 16/03/2024
+ *
+ * Description : Printing Numbers 1 to 10 ,Printing Odd Numbers using For Loop in JavaScript Programming Language Example with Explanation and Solution
+ */
+
+// 1 to 10
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+/* Output:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+*/
+
+// Explanation of the code above
+// i = 1
+// i <= 10
+// i++ is equivalent to i + 1 = 2, then i becomes 2
+// So, loop will continue until i is equal to 10
