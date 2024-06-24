b4a5c698e7e45f8ebe5e984e0d0978a5c01a1f10
musaraf0101 <nowsathmusaraf037@gmail.com>
2024-06-24 22:18:34 +0530

    Find Large Number Among three number

---
+++ Java/Easy - Basic Programs/findLargestNumber.java
@@ -0,0 +1,16 @@
+/*
+ * Author : musaraf
+ * Email : nowsathmusaraf037@gmail.com
+ * Date : 24/06/2024
+ * Description : Find Large Number Among three number
+ */
+
+ import java.util.Scanner;
+ public class LargeNumberAmongThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+    }
+
+}
