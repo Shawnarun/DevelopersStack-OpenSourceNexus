6ef9c661ca65e1129b931b6577619b29e1b4eb14
musaraf0101 <nowsathmusaraf037@gmail.com>
2024-06-24 22:23:30 +0530

    FEAT : Find Large Number Among three number

--- Java/Easy - Basic Programs/findLargestNumber.java
+++ Java/Easy - Basic Programs/findLargestNumber.java
@@ -10,6 +10,25 @@
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter first number : ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second Number : ");
+        int num2 = sc.nextInt();
+
+        System.out.println("Enter Third Number : ");
+        int num3 = sc.nextInt();
+
+        if(num1>num2 && num1 >num3){
+            System.out.println("Number "+num1+ " is Largest Number");
+        }
+        else if(num2>num1 && num2 > num3){
+            System.out.println("Number "+num2+" is Largest Number");
+        }
+        else{
+            System.out.println("Number "+num3+" is Largest Number");
+        }
+
         sc.close();
     }
 
