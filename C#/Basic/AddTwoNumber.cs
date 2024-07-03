1a503e35dcb010b1bf85fb1f2e97b49dbc20a2b0
musaraf0101 <nowsathmusaraf037@gmail.com>
2024-07-03 23:37:22 +0530

    FEAT : Sum Two Number C#

---
+++ C#/Basic/AddTwoNumber.cs
@@ -0,0 +1,19 @@
+/*
+ * Author : musaraf
+ * Email : nowsathmusaraf037@gmail.com
+ * Date : 03/07/2024
+ * Description : Get User input And Sum 2 number
+ */
+
+
+namespace AddTwoNumber{
+    internal class program{
+        static void main(string [] args){
+            int a = onvert.ToInt32(Console.ReadLine());
+            int b = onvert.ToInt32(Console.ReadLine());
+            int c = a + b;
+
+            Console.WriteLine(c);
+        }
+    }
+}
