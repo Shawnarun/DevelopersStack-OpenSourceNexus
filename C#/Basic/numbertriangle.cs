9e87c15766ef0a255039c43c30197f5c1b757ae5
musaraf0101 <nowsathmusaraf037@gmail.com>
2024-07-06 19:10:49 +0530

    FEAT : Number Triangle

---
+++ C#/Basic/numbertriangle.cs
@@ -0,0 +1,19 @@
+/*
+* Author : musaraf
+* Email : nowsathmusaraf037@gmail.com
+* Date : 06/07/2024
+* Description : Number Triangle
+*/
+
+namespace numbertriangle{
+    class program{
+        static void main(string [] args){
+            for(int i=0;i<10;i++){
+                for(int j=0;j<=i;j++){
+                    Console.WriteLine("*");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
