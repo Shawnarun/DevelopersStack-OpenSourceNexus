0911eb8457a891023a785509c63c1392cfc2d5e3
Anojananoj2000 <140131437+Anojananoj2000@users.noreply.github.com>
2025-02-11 08:58:51 +0530

    FEAT - Leap Year

---
+++ Java/Easy - Basic Programs/LeapYear.java
@@ -0,0 +1,27 @@
+/*
+Author: Anojananoj2000
+Email:anojananoj2000@gmail.com
+Date:10/02/2025
+Describtion:Find the year Leap year or not
+*/
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+
+        boolean isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+
+        if (isLeapYear) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is not a Leap Year.");
+        }
+
+        scanner.close();
+    }
+}
